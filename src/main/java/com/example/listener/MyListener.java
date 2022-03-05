3d48a97c719c7f2366a7fe2964529c6570ff72b5
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2022-03-05 16:48:14 -0300

    registrando listenner

--- src/main/java/com/example/listener/MyListener.java
+++ src/main/java/com/example/listener/MyListener.java
@@ -1,16 +1,24 @@
 package com.example.listener;
 
+import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class MyListener implements ServletRequestListener {
     @Override
-    public void requestDestroyed(ServletRequestEvent sre) {
-        ServletRequestListener.super.requestDestroyed(sre);
+    public void requestDestroyed(ServletRequestEvent e) {
+        System.out.println("Request destruida");
     }
 
     @Override
-    public void requestInitialized(ServletRequestEvent sre) {
-        ServletRequestListener.super.requestInitialized(sre);
+    public void requestInitialized(ServletRequestEvent e) {
+       // toda vez que uma request for iniciada será inserido um atributo nela
+        // assim todo o JSP ou servletque tiverem acesso a esta request poderão acessar este atributo
+        ServletRequest request = e.getServletRequest(); // request sendo criada
+        // inserindo atributo na request
+        request.setAttribute("user", "1234");
+        System.out.println("Request inicializada");
     }
 }
